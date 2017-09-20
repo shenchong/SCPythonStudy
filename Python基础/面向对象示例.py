fcbf9bfa555e5ab61d308132535599c2302b3a08
shenchong <569471293@qq.com>
2017-09-21 00:41:23 +0800

    工厂方法设计模式

--- Python基础/面向对象示例.py
+++ Python基础/面向对象示例.py
@@ -1,7 +1,31 @@
-# 设计4s店
 # -*- coding: UTF-8 -*-
 
-class CarStore(object):
+class Store(object):
+    def select_car(self):
+        pass
+
+    def order(self, car_name):
+        return self.select_car()
+
+
+class BMWCarStore(Store):
+    def __init__(self):
+        self.factory = BMWFactory()
+
+    def order(self, car_name):
+        return self.factory.select_car_by_name(car_name)
+
+# 使用函数将CarStore类解耦
+class BMWFactory(object):
+    def select_car_by_name(self, car_name):
+        if car_name == "530Li":
+            return Lavida()
+        elif car_name == "mini":
+            return Passat()
+        elif car_name == "X6":
+            return Magotan()
+
+class DZCarStore(Store):
     def __init__(self):
         self.factory = Factory()
 
@@ -37,13 +61,76 @@ class Passat(Car):
 class Magotan(Car):
     pass
 
-car_store = CarStore()
+car_store = DZCarStore()
 car = car_store.order("朗逸")
 car.move()
 car.music()
 car.stop()
 
 
+# 注：下面该模式为工厂方法模式，Store中order方法的self.select_car()，会调用子类中的方法
+# -*- coding: UTF-8 -*-
+
+class Store(object):
+    def select_car(self):
+        pass
+
+    def order(self, car_name):
+        return self.select_car(car_name)
 
 
-		
+class BMWCarStore(Store):
+
+    def select_car(self, car_name):
+        return BMWFactory.select_car_by_name(car_name)
+
+# 使用函数将CarStore类解耦
+class BMWFactory(object):
+    @classmethod
+    def select_car_by_name(self, car_name):
+        if car_name == "530Li":
+            return Lavida()
+        elif car_name == "mini":
+            return Passat()
+        elif car_name == "X6":
+            return Magotan()
+
+class CarStore(Store):
+    def select_car(self, car_name):
+        return Factory.select_car_by_name(car_name)
+
+# 使用函数将CarStore类解耦
+class Factory(object):
+    @classmethod
+    def select_car_by_name(self, car_name):
+        if car_name == "朗逸":
+            return Lavida()
+        elif car_name == "帕萨特":
+            return Passat()
+        elif car_name == "迈腾":
+            return Magotan()
+
+class Car(object):
+    def move(self):
+        print("-- 车在移动 --")
+
+    def music(self):
+        print("-- 车在播放音乐 --")
+
+    def stop(self):
+        print("-- 停车 --")
+
+class Lavida(Car):
+    pass
+
+class Passat(Car):
+    pass
+
+class Magotan(Car):
+    pass
+
+car_store = CarStore()
+car = car_store.order("朗逸")
+car.move()
+car.music()
+car.stop()
